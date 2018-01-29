29fcd680660d7aeed3145bd2cb17258f31787336
Kasp5953 <kasp5953@edu.easj.dk>
2018-01-29 12:59:19 +0100

    adds new public class Classroom
    class has instance fields and constructor initialising:
    - string _className
    - List<Student> _classList
    - DateTime _semesterStart
    constructor takes 1 parameter:
    - string className

---
+++ ClassRoom/Classroom.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom {
+    public class Classroom {
+        private string _className;
+        private List<Student> _classList;
+        private DateTime _semesterStart;
+
+        public Classroom(string className) {
+            _className = className;
+            _classList = new List<Student>();
+            _semesterStart = new DateTime();
+        }
+    }
+}
